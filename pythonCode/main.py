f4b3bc0e035247547ee70098c2fe3b4e7dab6f3b
lengfeng <lengfeng85@163.com>
2020-11-01 08:11:09 +0800

    add first python code

---
+++ pythonCode/main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def myMainFunc():
+    x = input("x:");
+    y = input("y:");
+    z = int(x) * int(y)
+    name = input("your name:");
+    print("hello, ", name, "! Your result is ", z)
+    input("Press Enter to exit!")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    myMainFunc()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
